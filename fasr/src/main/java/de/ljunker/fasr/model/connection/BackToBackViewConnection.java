24d1f3adb561c872231df8037dbde8f239dd77fd
Lars Junker <larsjunker92@gmail.com>
2025-07-24 10:44:57 +0200

    feat: add EightChain formation template and BackToBackViewConnection
    - Introduced `EightChain` formation in `FormationTemplates`.
    - Added `BackToBackViewConnection` class for dancer relationships.
    - Updated `GraphMatcherTest` with a new isomorphism test for `EightChain`.

---
+++ fasr/src/main/java/de/ljunker/fasr/model/connection/BackToBackViewConnection.java
@@ -0,0 +1,19 @@
+package de.ljunker.fasr.model.connection;
+
+import de.ljunker.fasr.model.Dancer;
+import java.util.HashSet;
+import java.util.Set;
+import javax.swing.text.View;
+
+public class BackToBackViewConnection extends ViewConnection {
+  private final Set<Dancer> dancers = new HashSet<>();
+
+  public BackToBackViewConnection(Dancer d1, Dancer d2) {
+    dancers.add(d1);
+    dancers.add(d2);
+  }
+
+  public Set<Dancer> getDancers() {
+    return dancers;
+  }
+}
